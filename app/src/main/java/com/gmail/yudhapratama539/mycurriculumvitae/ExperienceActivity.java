b4110341bbc0a06b31995044e4d336fc7129788e
kikiosha <yudhapratama539@gmail.com>
2017-10-30 20:54:52 +0700

    Fix Collapsing Toolbar

--- app/src/main/java/com/gmail/yudhapratama539/mycurriculumvitae/ExperienceActivity.java
+++ app/src/main/java/com/gmail/yudhapratama539/mycurriculumvitae/ExperienceActivity.java
@@ -1,22 +1,27 @@
 package com.gmail.yudhapratama539.mycurriculumvitae;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class ExperienceActivity extends AppCompatActivity {
+    private Button buttonRamalanJodoh, buttonDompetku, buttonSquline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experience);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        buttonRamalanJodoh=(Button)findViewById(R.id.btn_ramalanjodoh);
+        buttonDompetku=(Button)findViewById(R.id.btn_dompetku);
+        buttonSquline=(Button)findViewById(R.id.btn_squline);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -28,21 +33,32 @@ public class ExperienceActivity extends AppCompatActivity {
                 ContextCompat.getColor(this, R.color.white));
         collapsingToolbarLayout.setExpandedTitleColor(
                 ContextCompat.getColor(this, R.color.colorPrimary));
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        buttonRamalanJodoh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.kudkud32.ramalanjodoh"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
-        if (id == android.R.id.home){
-            //finish();
-            Intent intent=new Intent(ExperienceActivity.this, MainActivity.class);
-            startActivity(intent);
-        }
+        buttonDompetku.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.rackspira.dompetku"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
+        buttonSquline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.squline.student&hl=en"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
